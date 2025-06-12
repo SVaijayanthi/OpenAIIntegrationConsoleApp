3b6c1b140e9cccfd5d844533f7c29b7a97d59247
SVaijayanthi <Vaijayanthi2907@gmail.com>
2025-06-12 15:18:08 +0530

    initial

---
+++ OpenAIChatENG/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAIChatENG
+{
+    internal class Constants
+    {
+        public const string OpenAIKey = "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";
+    }
+}
